9aa8c8ed2c1c0ca5651312f3edb8f681cc638b21
mario1980miranda <mario_chan15@hotmail.com>
2022-08-04 13:47:01 -0400

    styling filter component with datepicker

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -1,9 +1,31 @@
+import 'flatpickr/dist/themes/material_green.css';
 import './styles.css';
 
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+import flatpickrLib from 'flatpickr';
+import FlatPicker from 'react-flatpickr';
+
+flatpickrLib.localize(Portuguese);
+
 function Filter() {
+  const onChangeDate = (dates: Date[]) => {
+    console.log(dates);
+  };
+
   return (
     <div className="filter-container base-card">
-      <h1>filter</h1>
+      <FlatPicker
+        options={{ mode: 'range', dateFormat: 'd/m/Y', showMonths: 2 }}
+        className="filter-input"
+        onChange={onChangeDate}
+        placeholder="Selecione um período"
+      />
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="MALE">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outro</option>
+      </select>
     </div>
   );
 }
