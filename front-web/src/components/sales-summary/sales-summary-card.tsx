cbadd49fbf24df72dad8a77d95b4ffb8b51e47b5
mario1980miranda <mario_chan15@hotmail.com>
2022-08-05 10:15:40 -0400

    Sales summary cards values and styles

--- front-web/src/components/sales-summary/sales-summary-card.tsx
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -1,5 +1,14 @@
+import './styles.css';
+import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
+
 function SalesSummaryCard() {
-  return <div className="sales-summary-card base-card"></div>;
+  return (
+    <div className="sales-summary-card base-card">
+      <AvatarIcon />
+      <h3 className="sales-summary-card-value">534.00</h3>
+      <span className="sales-summary-card-label">Média</span>
+    </div>
+  );
 }
 
 export default SalesSummaryCard;
