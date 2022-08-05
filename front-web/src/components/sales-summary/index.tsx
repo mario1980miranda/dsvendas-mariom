6b03905d5b94f8cd320dd7cb3dff95b989ee3588
mario1980miranda <mario_chan15@hotmail.com>
2022-08-05 10:23:24 -0400

    Sales summary cards subcomponents with dynamic values

--- front-web/src/components/sales-summary/index.tsx
+++ front-web/src/components/sales-summary/index.tsx
@@ -1,13 +1,18 @@
 import SalesSummaryCard from './sales-summary-card';
+import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
+import { ReactComponent as SyncIcon } from '../../assets/sync-icon.svg';
+import { ReactComponent as BarChartIcon } from '../../assets/bar-chart-icon.svg';
+import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
+
 import './styles.css';
 
 function SalesSummary() {
   return (
     <div className="sales-summary-container">
-      <SalesSummaryCard />
-      <SalesSummaryCard />
-      <SalesSummaryCard />
-      <SalesSummaryCard />
+      <SalesSummaryCard value={430} label="Média" icon={<DoneIcon />} />
+      <SalesSummaryCard value={300} label="Média" icon={<SyncIcon />} />
+      <SalesSummaryCard value={520} label="Média" icon={<BarChartIcon />} />
+      <SalesSummaryCard value={832} label="Média" icon={<AvatarIcon />} />
     </div>
   );
 }
